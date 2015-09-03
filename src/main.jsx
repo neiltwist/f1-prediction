04b206c01af07fe01bf0569b03a9090730bed3f5
Neil Twist <neiltwist@gmail.com>
2015-09-03 19:33:38 +0100

    included chartjs and data processing

--- src/main.jsx
+++ src/main.jsx
@@ -2,33 +2,19 @@
 var React = require('react'),
     _ = require('lodash'),
     d3 = require('d3'),
+    Chartjs = require('Chart.js'),
     drawers = require('./drawers.jsx'),
     Controls = require('./controls.jsx'),
     meta = require('./meta.jsx');
 
-var H1BGraph = React.createClass({
+var LapTimes = React.createClass({
     loadRawData: function () {
         var dateFormat = d3.time.format("%m/%d/%Y");
-        d3.csv(this.props.url)
-          .row(function (d) {
-              if (!d['base salary']) {
-                  return null;
-              }
-
-              return {employer: d.employer,
-                      submit_date: dateFormat.parse(d['submit date']),
-                      start_date: dateFormat.parse(d['start date']),
-                      case_status: d['case status'],
-                      job_title: d['job title'],
-                      base_salary: Number(d['base salary']),
-                      salary_to: d['salary to'] ? Number(d['salary to']) : null,
-                      city: d.city,
-                      state: d.state};
-          }.bind(this))
+        d3.json(this.props.url)
           .get(function (error, rows) {
               if (error) {
-                  console.error(error);
-                  console.error(error.stack);
+                console.error(error);
+                console.error(error.stack);
               }else{
                   this.setState({rawData: rows});
               }
@@ -39,9 +25,57 @@ var H1BGraph = React.createClass({
         this.loadRawData();
     },
 
+    componentDidMount: function () {
+      this.showChart();
+    },
+
+    componentDidUpdate: function () {
+      this.showChart();
+    },
+
+    showChart: function () {
+      if (!this.myLineChart && this.filteredData) {
+        var options = {};
+        var data = {
+          "labels": [],
+          "datasets": []
+        };
+        var maxLaps = 0;
+        this.filteredData.forEach(function (driver) {
+          console.log(driver.laps.length + " > " + maxLaps);
+          if (driver.laps.length > maxLaps) {
+            for (var i = maxLaps; i < driver.laps.length; i++) {
+              data.labels.push(i+1);
+            }
+            maxLaps = driver.laps.length;
+          }
+          var lineData = {
+            label: driver.name,
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: []
+          };
+          driver.laps.forEach(function (lap) {
+            lineData.data.push(lap.laptime);
+          });
+          data.datasets.push(lineData);
+        });
+        console.log(data);
+        console.log(React);
+        this.ctx = React.findDOMNode(this.refs.driverChart).getContext("2d");
+        this.myLineChart = new Chart(ctx).Line(this.filteredData, options);
+      }
+    },
+
     getInitialState: function () {
-        return {rawData: [],
-                dataFilter: function () { return true; }};
+        return {
+          rawData: [],
+          dataFilter: function () { return true; }
+        };
     },
 
     updateDataFilter: function (filter) {
@@ -50,34 +84,32 @@ var H1BGraph = React.createClass({
 
     render: function () {
         if (!this.state.rawData.length) {
+          this.filteredData = null;
             return (
-                <h2>Loading data about 81,000 H1B visas in the software industry</h2>
+                <h2>Loading Lap Times</h2>
             );
         }
 
-        var params = {
-            bins: 20,
-            width: 500,
-            height: 500,
-            axisMargin: 83,
-            topMargin: 10,
-            bottomMargin: 5,
-            value: function (d) { return d.base_salary; }
-        },
-            fullWidth = 700;
+        this.filteredData = this.state.rawData.filter(this.state.dataFilter);
 
-
-        var filteredData = this.state.rawData.filter(this.state.dataFilter);
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-md-12" ref="chart-mount-point">
+                      <canvas id="lap-times-canvas" ref="driverChart"
+                        width="400" height="400" />
+                    </div>
+                </div>
+            </div>
+        );
 
         return (
             <div>
                 <meta.Title data={filteredData} />
                 <meta.Description data={filteredData} allData={this.state.rawData} />
                 <div className="row">
-                    <div className="col-md-12">
-                        <svg width={fullWidth} height={params.height}>
-                            <drawers.Histogram {...params} data={filteredData} />
-                        </svg>
+                    <div className="col-md-12" ref="chart-mount-point">
+
                     </div>
                 </div>
                 <Controls data={this.state.rawData} updateDataFilter={this.updateDataFilter} />
@@ -88,6 +120,6 @@ var H1BGraph = React.createClass({
 });
 
 React.render(
-    <H1BGraph url="data/h1bs.csv" />,
-    document.querySelectorAll('.h1bgraph')[0]
+    <LapTimes url="data/lap-times.json" />,
+    document.querySelectorAll('.lap-times')[0]
 );
